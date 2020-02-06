a5fbc774fbb10572af142d3caf98bbe3f5e32d18
Jordan Black <jblack530@gmail.com>
2020-02-06 13:51:42 -0800

    finished server, and implementation of backend sending payment tokens to stripe api

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout'
-
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_BoxtTc1QNzZLYIvfU3tQy5U200j5ZIJrym";
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful');
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token
+          }
+        }).then(response => {
+          alert('Payment Successful')
+        }).catch(error => {
+          console.log('Payment error: ', JSON.parse(error))
+          alert(
+            'There was an issue with your payment. Please make sure you are using the test credit card information provided'
+          )
+        })
     }
 
     return (
