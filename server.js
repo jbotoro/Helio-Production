5cf0407aeb79fe3c616a3f4bacfc7c8ec2b7cf9a
Jordan Black <jblack530@gmail.com>
2020-02-10 09:48:27 -0800

    added new info to server.js and changed git ignore

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const enforce = require('express-sslify');
 
-if (process.env.NODE_ENV !== "production") require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,24 +16,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log("Server running on port " + port);
+  console.log('Server running on port ' + port);
 });
 
-app.post("/payment", (req, res) => {
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
+app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: "usd"
+    currency: 'usd'
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
