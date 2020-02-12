facc5a407d84adf2f1fa45a88a74be63722e9598
Jordan Black <jblack530@gmail.com>
2020-02-12 11:42:02 -0800

    added graphql folder and resolvers.js and added it to indexjs

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-boost';
+import { resolvers, typeDefs } from './graphql/resolvers'
 
 import './index.css';
 import App from './App';
@@ -21,8 +22,17 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  resolvers,
+  typeDefs
 })
+
+client.writeData({
+  data: {
+    cartHidden: true
+  }
+})
+
 
 
 
