c3fc2307029b387f20c0914e9e2baa9d3564ef94
Jordan Black <jblack530@gmail.com>
2020-02-11 16:19:31 -0800

    added apollo to index js

--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-boost';
 
 import './index.css';
 import App from './App';
 
 import {store ,persistor} from "./redux/store";
 
+const httpLink = createHttpLink({
+  uri: 'https://helio-live.herokuapp.com/'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
